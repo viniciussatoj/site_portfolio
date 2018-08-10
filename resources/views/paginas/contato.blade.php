c8830f3a0c960e502f9ffcc1b5c80aa36ea0b67b
vinicius sato <vinicius.sato@institutosoma.org.br>
2018-08-10 11:54:52 -0300

    corrigi o layout das redes sociais

--- resources/views/paginas/contato.blade.php
+++ resources/views/paginas/contato.blade.php
@@ -1,16 +1,16 @@
 @extends('layout')
 @section('conteudo')
-    <div class="col-9 container">
-        <div class="row justify-content-around">
-            <div class="contato-container">
-                <span class="texto">Me mande um email ou entre em contato em alguma rede :)</span>
-                <div class="divisor-linha"></div>
-                <span class="email">vinicius.satoj@gmail.com</span>
-                <div class="redes-sociais">
-                    <a href="https://www.linkedin.com/in/vinicius-satoj/"><img src="img/linkedin.svg" /></a>
-                    <a href="https://medium.com/@vinicius.satoj"><img src="img/medium.svg" />
-                </div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="contato-container col-9">
+            <span class="texto">Me mande um email ou entre em contato em alguma rede :)</span>
+            <div class="divisor-linha"></div>
+            <span class="email"><a href="mailto:vinicius.satoj@gmail.com">vinicius.satoj@gmail.com</a></span>
+            <div class="redes-sociais">
+                <a href="https://www.linkedin.com/in/vinicius-satoj/"><img src="img/linkedin.svg" /></a>
+                <a href="https://medium.com/@vinicius.satoj"><img src="img/medium.svg" />
             </div>
         </div>
     </div>
+</div>
 @endsection
