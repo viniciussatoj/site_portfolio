14f8d2005f23032498853653e2a308330dfc04d5
vinicius sato <vinicius.sato@institutosoma.org.br>
2018-08-10 11:30:05 -0300

    foto adicionada, frase removida e conteúdo do sobre alterado

--- resources/views/paginas/sobre.blade.php
+++ resources/views/paginas/sobre.blade.php
@@ -5,20 +5,20 @@
                 <div class="col-9 ">
                     <div class="row">
                         <div class="col-md-6 col-sm-12">
-                            <img class="foto_pessoal" src="https://i.imgur.com/MxeEPMt.jpg" />
+                            <img class="foto_pessoal" src="{{ URL::to('/') }}/img/gerais/perfil.jpg" />
                         </div>
                         <div class="col-md-6 col-sm-12 descricao_pessoal">
                             <p class="paragrafo_descricao">
-                                Olá, meu nome é <span class="destaque-negrito">Vinicius</span>, sou designer pelo curso de Design da <span class="destaque-verde">UNESP Bauru</span> com foco em <span class="destaque-vermelho">UX/UI.</span>
+                                Olá, meu nome é Vinicius, sou designer pelo curso de <span class="destaque-negrito">Design da UNESP Bauru</span> com foco em <span class="destaque-negrito">UX/UI.</span>
                             </p>
                             <p class="paragrafo_descricao">
-                                Durante a faculdade eu atuei na organização de do <span class="destaque-azul">Interdesigners</span>, evento regional para estudantes de design e o <span class="destaque-azul">BUG - Bauru United for Gamers</span>, evento focado na área acadêmica e mercado de jogos da região.
+                                Durante a faculdade eu atuei na organização de do Interdesigners, evento regional para estudantes de design e o BUG - Bauru United for Gamers, evento focado na área acadêmica e mercado de jogos da região.
                             </p>
                             <p class="paragrafo_descricao">
-                                Participei por 3 anos do <span class="destaque-verde">LTIA - Laboratório de Tecnologia da Informação Aplicada</span> onde eu comecei atuando como <span class="destaque-vermelho">Game Designer</span> em projetos pessoais com a equipe e eventualmente migrei para projetos internos encomendados pela Motorola e trabalhei com <span class="destaque-vermelho">Motion</span> e <span class="destaque-vermelho">User Interface</span>.
+                                Participei por 4 anos do LTIA - Laboratório de Tecnologia da Informação Aplicada onde eu comecei atuando como Game Designer em projetos pessoais com a equipe e eventualmente migrei para projetos internos encomendados pela Motorola e trabalhei com Motion e User Interface.
                             </p>
                             <p class="paragrafo_descricao">
-                                Atualmente eu tenho me focado nas áreas de <span class="destaque-vermelho">UX, Product Design e Service Design</span> e <span class="destaque-negrito">estou buscando oportunidades de atuação na área.</span>
+                                Atualmente eu tenho me focado nas áreas de <span class="destaque-negrito">UX, Product Design e Service Design</span> e estou buscando oportunidades de atuação na área.
                             </p>
                             <p class="paragrafo_social">
                             Eu estou sempre aberto a projetos de cunho social, precisando de um designer ou programador me manda um mensagem :)
